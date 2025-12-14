94849d765d2744cb16d95b5d7f026066053d9973
Adam Martula <martulaadam@gmail.com>
2025-12-14 13:48:42 +0100

    Add sample event

--- app/src/main/java/io/github/acaimingus/worqspace/ui/calendar/CalendarFragment.java
+++ app/src/main/java/io/github/acaimingus/worqspace/ui/calendar/CalendarFragment.java
@@ -4,12 +4,17 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
 
 import androidx.appcompat.widget.PopupMenu;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+
+import com.applandeo.materialcalendarview.CalendarDay;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
 
 import io.github.acaimingus.worqspace.R;
 import io.github.acaimingus.worqspace.databinding.FragmentCalendarBinding;
@@ -20,11 +25,26 @@ public class CalendarFragment extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
+        // Create the binding
         binding = FragmentCalendarBinding.inflate(inflater, container, false);
 
-        FloatingActionButton fab = binding.getRoot().findViewById(R.id.fab_add_event);
+        // Create a sample calendar event
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.HOUR_OF_DAY, 20);
+        calendar.set(Calendar.MINUTE, 0);
 
+        List<CalendarDay> events = new ArrayList<>();
+
+        CalendarDay day = new CalendarDay(calendar);
+        day.setImageResource(R.drawable.calendar);
+
+        events.add(day);
+        binding.calendarView.setCalendarDays(events);
+
+        // Create a floating action button for the calendar
+        FloatingActionButton fab = binding.getRoot().findViewById(R.id.fab_add_event);
         fab.setOnClickListener(view -> {
+            // Create a popup menu for the floating action button
             PopupMenu popupMenu = new PopupMenu(requireContext(), view);
             popupMenu.getMenuInflater().inflate(R.menu.menu_add_calendar_event, popupMenu.getMenu());
             popupMenu.setForceShowIcon(true);
