4212b9d64123dc01c4196704690a823f023f6239
Adam Martula <martulaadam@gmail.com>
2025-12-14 12:41:03 +0100

    Add calendar library

--- app/src/main/java/io/github/acaimingus/worqspace/ui/calendar/CalendarFragment.java
+++ app/src/main/java/io/github/acaimingus/worqspace/ui/calendar/CalendarFragment.java
@@ -4,11 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
-
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProvider;
 
 import io.github.acaimingus.worqspace.databinding.FragmentCalendarBinding;
 
@@ -18,15 +15,8 @@ public class CalendarFragment extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        CalendarViewModel calendarViewModel =
-                new ViewModelProvider(this).get(CalendarViewModel.class);
-
         binding = FragmentCalendarBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
-
-        final TextView textView = binding.textCalendar;
-        calendarViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
-        return root;
+        return binding.getRoot();
     }
 
     @Override
