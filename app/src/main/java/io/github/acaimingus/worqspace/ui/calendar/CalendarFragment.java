9d8ebc53a5d64f176f29ab1079e1fc9a7d684a09
Adam Martula <martulaadam@gmail.com>
2025-12-14 12:58:25 +0100

    Readd floating button in calendar

--- app/src/main/java/io/github/acaimingus/worqspace/ui/calendar/CalendarFragment.java
+++ app/src/main/java/io/github/acaimingus/worqspace/ui/calendar/CalendarFragment.java
@@ -4,9 +4,12 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import io.github.acaimingus.worqspace.R;
 import io.github.acaimingus.worqspace.databinding.FragmentCalendarBinding;
 
 public class CalendarFragment extends Fragment {
@@ -16,6 +19,13 @@ public class CalendarFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         binding = FragmentCalendarBinding.inflate(inflater, container, false);
+
+        FloatingActionButton fab = binding.getRoot().findViewById(R.id.fab_add_event);
+
+        fab.setOnClickListener(view -> {
+            Toast.makeText(getContext(), "Create new event", Toast.LENGTH_SHORT).show();
+        });
+
         return binding.getRoot();
     }
 
